ed6e1384a9b2ec1ac87c3c1a88d51b5cd8dd992a
Tomasz <justupgrade@o2.pl>
2015-04-13 15:53:17 +0200

    images exercise

--- advanced-6/image/index.php
+++ advanced-6/image/index.php
@@ -8,14 +8,39 @@
 	if($_SERVER['REQUEST_METHOD'] === 'POST') {
 		if(isset($_POST['img_url'])) {
 			$url = $_POST['img_url'];
-			//find out type of image:
-			$img = imagecreatefromjpeg($url);
-			$str = "Coders Lab";
-			$px = (imagesx($img) - 7.5*strlen($str));
-			$py = (imagesy($img) - 15);
-			imagestring($img, 3, $px, $py, $str,null);
-			Header('Content-type: image/jpeg');
-			imagejpeg($img);
+			//find out the type:
+			$pos = strrpos($url, ".");
+			$type = substr($url, $pos+1);
+			
+			$logo = imagecreatefrompng("logo.png");
+
+			//load image
+			if($type = "jpg") $img = imagecreatefromjpeg($url);
+			elseif($type = "gif") $img = imagecreatefromgif($url);
+			//edit image
+			if($img) {
+				$str = "Coders Lab";
+				$px = (imagesx($img) - 7.5*strlen($str));
+				$py = (imagesy($img) - 15);
+				imagestring($img, 5, $px, $py, $str,null);
+				
+				imagecopymerge($img, $logo, imagesx($img)-imagesx($logo), imagesy($img)-imagesy($logo), 0, 0, imagesx($logo), imagesy($logo), 25);
+			}
+			
+			//send to browser
+			if($img && $type = "jpg") {
+				//Header('Content-type: image/jpeg');
+				//$file = imagejpeg($img);
+				file_put_contents('filename.jpeg',imagejpeg($img));
+				echo "<a href='filename.jpeg'>Download File </a>";
+			} elseif($img && $type="gif") {
+				//Header('Content-type: image/gif');
+				//$file = imagegif($img);
+				file_put_contents('filename.gif',imagegif($img));
+				//imagegif($img);
+				echo "<a href='filename.gif'>Download File </a>";
+			}
+			
 		}
 	}
 ?>
