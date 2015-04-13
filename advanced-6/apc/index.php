71aacf99deaa690c13b04a3ff44f4cfae638b4bc
Tomasz <justupgrade@o2.pl>
2015-04-13 16:09:19 +0200

    APC exercise

--- advanced-6/apc/index.php
+++ advanced-6/apc/index.php
@@ -1,1 +1,11 @@
 <?php
+	if(!apc_exists("blabla")){
+		apc_store("blabla",1);
+	}
+	echo "Number of visits: " . apc_fetch("blabla");
+	apc_inc("blabla");
+?>
+<html>
+<head>
+<title>APC</title></head>
+</html>
